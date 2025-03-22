ae87f42eeca25f076702218cd5fe30c33fbe8b6d
hitsan <soledewa2828@gmail.com>
2025-03-22 19:06:08 +0900

    add configurations

---
+++ hosts/spica/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-ld.url = "github:Mic92/nix-ld";
+    # this line assume that you also have nixpkgs as an input
+    nix-ld.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nix-ld, nixpkgs, ... }@inputs: {
+    nixosConfigurations.spica = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        nix-ld.nixosModules.nix-ld
+
+        { programs.nix-ld.dev.enable = true; }
+      ];
+    };
+  };
+}
