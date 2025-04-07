4a315ae6b0fba165aa1d1074dfe6e52c7d9fd932
hitsan <soledewa2828@gmail.com>
2025-04-07 22:00:34 +0900

    add stop aliase

--- shell/zsh.nix
+++ shell/zsh.nix
@@ -9,6 +9,7 @@
       ll = "ls -l";
       nix_switch = "sudo nixos-rebuild switch --flake ~/dotfiles#spica";
       home = "home-manager switch --flake ~/dotfiles#hitsan";
+      stop = "sudo systemctl suspend";      
     };
     oh-my-zsh = {
       enable = true;
