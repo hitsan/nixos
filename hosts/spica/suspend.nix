6ffd9ceea13487a144bbefbf66f1987d246c8bd8
hitsan <soledewa2828@gmail.com>
2025-04-08 22:29:30 +0900

    update suspend conf

--- hosts/spica/suspend.nix
+++ hosts/spica/suspend.nix
@@ -3,5 +3,13 @@
   services.logind.extraConfig = ''
     IdleAction=suspend
     IdleActionSec=10800
+    HandleLidSwitch=ignore
+    HandleLidSwitchExternalPower=ignore
+    HandleLidSwitchDocked=ignore
+    StopWhenSessionsEmpty=no
+    IdleActionIgnoreInhibited=yes
+    IdleActionIgnoreDocked=yes
+    IdleActionIgnoreOnLid=yes
+    IdleActionIgnoreOnExternalPower=yes
   '';
 }
