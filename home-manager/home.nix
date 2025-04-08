7149d6fd5d9a5a0779deaea0bb6a487d545c3936
hitsan <soledewa2828@gmail.com>
2025-04-08 22:21:01 +0900

    remove conf

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -33,7 +33,6 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
-    pkgs.zellij
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
