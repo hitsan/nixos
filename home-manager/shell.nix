151ede585cddc6ad0f005265e651717bc73a8e4a
hitsan <soledewa2828@gmail.com>
2025-04-10 00:13:13 +0900

    change prompt

--- home-manager/shell.nix
+++ home-manager/shell.nix
@@ -11,6 +11,9 @@
       home = "home-manager switch --flake ~/dotfiles#hitsan";
       stop = "sudo systemctl suspend";
     };
+    initExtra = "
+      PROMPT=\"%F{green}$%f \";
+    ";
 
     dotDir = ".config/zsh";
   };
