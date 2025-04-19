67ce7bb616590381be99a1715d3a57bc269744df
hitsan <soledewa2828@gmail.com>
2025-04-19 17:14:18 +0900

    fix sinblic files path

--- home-manager/editor/editor.nix
+++ home-manager/editor/editor.nix
@@ -1,10 +1,4 @@
 { shell, home_path, ... }:
-let
-  nvim_root = ./nvim;
-  nvim_config = ".config/nvim";
-  lua_config = "${nvim_config}/lua";
-  plugins_config = "${lua_config}/plugins";
-in
 {
   programs.neovim = {
     enable = true;
@@ -12,11 +6,9 @@ in
   programs.${shell}.shellAliases = {
     v = "nvim";
   };
-  home.file = {
-    "${nvim_config}/init.lua".source = "${nvim_root}/init.lua";
-    "${lua_config}/base.lua".source = "${nvim_root}/lua/base.lua";
-    "${plugins_config}/init.lua".source = "${nvim_root}/lua/plugins/init.lua";
-    "${plugins_config}/gitsigns.lua".source = "${nvim_root}/lua/plugins/gitsigns.lua";
+  home.file.".config/nvim" = {
+    source = ./nvim;
+    recursive = true;
   };
 }
 
