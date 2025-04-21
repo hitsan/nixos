4c5cb2888d43648e0d3d162efdfa7dabeea66602
hitsan <soledewa2828@gmail.com>
2025-04-21 22:00:55 +0900

    add config path

--- home-manager/git/lazygit/default.nix
+++ home-manager/git/lazygit/default.nix
@@ -1,9 +1,12 @@
-{ shell, ... }:
+{ shell, home_path, ... }:
 {
   programs.lazygit = {
     enable = true;
   };
   programs.${shell}.shellAliases = {
     laz = "lazygit";
+  };
+  home.file.".config/lazygit/config.yml" = {
+    source = "${home_path}/git/lazygit/config.yml";
   };
 }
