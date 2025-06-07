5dfb2f44b8dc12f8cea59e519406877ab5304503
hitsan <soledewa2828@gmail.com>
2025-06-07 21:49:02 +0900

    Add golang lsp

--- home-manager/editor/neovim/nvim/lua/plugins/lsp.lua
+++ home-manager/editor/neovim/nvim/lua/plugins/lsp.lua
@@ -2,6 +2,14 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local on_attach = function(_, bufnr)
+        local map = function(mode, lhs, rhs)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, noremap = true, silent = true })
+        end
+        map("n", "gd", vim.lsp.buf.definition)
+        map("n", "K", vim.lsp.buf.hover)
+      end
+
       require("lspconfig").rust_analyzer.setup({
         handlers = {
           ["textDocument/signatureHelp"] = function() end,
@@ -19,13 +27,19 @@ return {
             },
           },
         },
-        on_attach = function(_, bufnr)
-          local map = function(mode, lhs, rhs)
-            vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, noremap = true, silent = true })
-          end
-          map("n", "gd", vim.lsp.buf.definition)
-          map("n", "K", vim.lsp.buf.hover)
-        end,
+        on_attach = on_attach,
+      })
+
+      require("lspconfig").gopls.setup({
+        on_attach = on_attach,
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
+          },
+        },
       })
     end,
   },
