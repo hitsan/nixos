f2b1d8a1680b899d9d76af4c3ff0041c36ebb4b7
hitsan <soledewa2828@gmail.com>
2025-06-07 22:16:02 +0900

    Add syntax highlight

---
+++ home-manager/editor/neovim/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "vim", "vimdoc", "rust", "go", "bash", "nix", "typescript" },
+        auto_install = true,
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  },
+}
