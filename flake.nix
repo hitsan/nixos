6692caecea5ea92f753513615a592749f1215b04
hitsan <soledewa2828@gmail.com>
2025-06-16 22:26:59 +0900

    remove home-manager

--- flake.nix
+++ flake.nix
@@ -1,14 +1,10 @@
 {
-  description = "My flake";
+  description = "My NixOS";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nix-ld = {
       url = "github:Mic92/nix-ld";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     xremap.url = "github:xremap/nix-flake";
@@ -19,9 +15,7 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
     user = "hitsan";
-    home = "/home/${user}";
     root = builtins.toString ./.;
-    home_path = "${root}/home-manager";
     modules_path = "${root}/modules";
     hosts_path = "${root}/hosts";
     shell = "zsh";
@@ -31,18 +25,11 @@
       spica = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = {
-          inherit user home nix-ld modules_path xremap;
+          inherit user nix-ld modules_path xremap;
         };
 
         modules = [ ./hosts/spica ];
       };
-    };
-
-    homeConfigurations.${user} = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      extraSpecialArgs = { inherit user home shell home_path; };
-
-      modules = [ ./home-manager/home.nix ];
     };
   };
 }
