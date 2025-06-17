1bb29fb14b9dcf59539704c7f80dfa4f6b3fca7b
hitsan <soledewa2828@gmail.com>
2025-06-18 02:10:10 +0900

    update nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,21 +2,20 @@
   description = "My NixOS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nix-ld = {
       url = "github:Mic92/nix-ld";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-ld }:
+  outputs = { self, nixpkgs, nix-ld }:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
     user = "hitsan";
     root = builtins.toString ./.;
     modules_path = "${root}/modules";
-    hosts_path = "${root}/hosts";
     shell = "zsh";
   in
   {
