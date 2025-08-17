a369c939c2f308c1f8b5e562123946b30c4dc4c4
hitsan <soledewa2828@gmail.com>
2025-08-17 17:25:24 +0900

    change locale lang from ja to en

--- hosts/spica/configuration.nix
+++ hosts/spica/configuration.nix
@@ -20,7 +20,7 @@
   time.timeZone = "Asia/Tokyo";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "ja_JP.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "ja_JP.UTF-8";
