c94cde3860ce2370cb4020702b51ba976a7f66d8
hitsan <15902694+hitsan@users.noreply.github.com>
2026-02-06 03:15:56 +0900

    change 25.05 to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nix-ld = {
       url = "github:Mic92/nix-ld";
